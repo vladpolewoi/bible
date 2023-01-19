1507f7a786351caac7976a436ba08158cc5d1481
vlad polewoi <vlad.polewoi1@gmail.com>
2023-01-19 23:44:54 +0200

    mvp

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <ChapterView :chapter="chapter" />
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import ChapterView from "@/components/ChapterView.vue";
+import data from "@/data.json";
+
+const chapter = reactive(data.chapters[0]);
+</script>
+
+<style lang="scss">
+@import "./styles/main.scss";
+
+.container {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+}
+</style>
